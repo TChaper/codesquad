3b39f4a00edd7d504ae2edd93e478299089f2314
Tchaper <miracle777z@gmail.com>
2020-12-12 01:22:45 +0900

    i -> j 변경

--- sources/step-2.py
+++ sources/step-2.py
@@ -13,30 +13,31 @@
 def cube(array, ent):
     for i in array:
         for j in i:
-            print(i, end = " ")
+            print(j, end = " ")
         print("")
 
 def inven():
     array = [["R","R","W"],["G","C","W"],["G","B","B"]]
     cube(array, False)
-    entry = input().split()
-    for ent in entry:
-        if ent == "U":
-            array[0][0], array[0][1], array[0][2] = array[0][1], array[0][2], array[0][0]
-        elif ent == "U'":
-            array[0][0], array[0][1], array[0][2] = array[0][2], array[0][0], array[0][1]
-        elif ent == "R":
-            array[0][2], array[1][2], array[2][2] = array[1][2], array[2][2], array[0][2]
-        elif ent == "R'":
-            array[0][2], array[1][2], array[2][2] = array[2][2], array[0][2], array[1][2]
-        elif ent == "L":
-            array[0][0], array[1][0], array[2][0] = array[2][0], array[0][0], array[1][0]
-        elif ent == "L'":
-            array[0][0], array[1][0], array[2][0] = array[1][0], array[2][0], array[0][0]
-        elif ent == "B":
-            array[2][0], array[2][1], array[2][2] = array[2][2], array[2][0], array[2][1]
-        elif ent == "B'":
-            array[2][0], array[2][1], array[2][2] = array[2][1], array[2][2], array[2][0]
-        return
-        cube(array)
+    while True:
+        entry = input().split()
+        for ent in entry:
+            if ent == "U":
+                array[0][0], array[0][1], array[0][2] = array[0][1], array[0][2], array[0][0]
+            elif ent == "U'":
+                array[0][0], array[0][1], array[0][2] = array[0][2], array[0][0], array[0][1]
+            elif ent == "R":
+                array[0][2], array[1][2], array[2][2] = array[1][2], array[2][2], array[0][2]
+            elif ent == "R'":
+                array[0][2], array[1][2], array[2][2] = array[2][2], array[0][2], array[1][2]
+            elif ent == "L":
+                array[0][0], array[1][0], array[2][0] = array[2][0], array[0][0], array[1][0]
+            elif ent == "L'":
+                array[0][0], array[1][0], array[2][0] = array[1][0], array[2][0], array[0][0]
+            elif ent == "B":
+                array[2][0], array[2][1], array[2][2] = array[2][2], array[2][0], array[2][1]
+            elif ent == "B'":
+                array[2][0], array[2][1], array[2][2] = array[2][1], array[2][2], array[2][0]
+            return
+            cube(array,ent)
 inven()
