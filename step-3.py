36feac4dfcca25fc90bf43b8534e755348c9253c
Tchaper <miracle777z@gmail.com>
2020-12-12 02:14:29 +0900

    초기 상태 출력

--- step-3.py
+++ step-3.py
@@ -5,6 +5,17 @@
 출력: 큐브의 6면을 펼친 상태로 출력한다.
 Q를 입력받으면 프로그램을 종료하고, 조작 받은 명령의 갯수를 출력시킨다.
 
+def inven():
+  def inven():
+    array = [[['B', 'B', 'B'], ['B', 'B', 'B'], ['B', 'B', 'B'],
+['R', 'R', 'R'], ['R', 'R', 'R'], ['R', 'R', 'R'],
+['O', 'O', 'O'], ['O', 'O', 'O'], ['O', 'O', 'O'],
+['Y', 'Y', 'Y'], ['Y', 'Y', 'Y'], ['Y', 'Y', 'Y'],
+['W', 'W', 'W'], ['W', 'W', 'W'], ['W', 'W', ‘W’],
+[‘G’, 'G', 'G'], ['G', 'G', 'G'], ['G', 'G', 'G']]]
+    cube(array, False)
+    while True:
+        entry = input("CUBE>
 for i in array:
   for j in i:
     for k in j:
