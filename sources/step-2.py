d72ad0212a2910f44799cb01de29f45998fd6891
Tchaper <miracle777z@gmail.com>
2020-12-12 01:47:33 +0900

    주석 삭제 및 프롬프트  표시

--- sources/step-2.py
+++ sources/step-2.py
@@ -1,16 +1,6 @@
-# U  가장 윗줄을 왼쪽으로 한 칸 밀기 RRW - RWR
-# U' 가장 윗줄을 오른쪽으로 한 칸 밀기 RRW - WRR
-# R  가장 오른쪽 줄을 위로 한 칸 밀기 WWB - WBW
-# R' 가장 오른쪽 줄을 아래로 한 칸 밀기 WWB - BWW
-# L  가장 왼쪽 줄을 아래로 한 칸 밀기 RGG - GRG (L의 경우 R과 방향이 반대임을 주의한다.)
-# L' 가장 왼쪽 줄을 위로 한 칸 밀기 RGG - GGR
-# B  가장 아랫줄을 오른쪽으로 한 칸 밀기 GBB - BGB (B의 경우도 U와 방향이 반대임을 주의한다.)
-# B' 가장 아랫줄을 왼쪽으로 한 칸 밀기 GBB - BBG
-# Q  Bye~를 출력하고 프로그램을 종료한다.
-# R R W
-# G C W
-# G B B
 def cube(array, ent):
+    if ent:
+        print("{}".format(ent))
     for i in array:
         for j in i:
             print(j, end = " ")
@@ -20,7 +10,8 @@ def inven():
     array = [["R","R","W"],["G","C","W"],["G","B","B"]]
     cube(array, False)
     while True:
-        entry = input().split()
+        print("")
+        entry = input("CUBE>" ).split()
         for ent in entry:
             if ent == "U":
                 array[0][0], array[0][1], array[0][2] = array[0][1], array[0][2], array[0][0]
@@ -41,5 +32,6 @@ def inven():
             elif ent == "Q":
                 print("Bye~")
                 return
+            print("")
             cube(array,ent)
 inven()
